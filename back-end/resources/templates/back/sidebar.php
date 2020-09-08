a825dbcfe6032ba02d29198e105c37ca4d7828b3
Salma Belassiri <salmabelassiri@gmail.com>
2020-09-08 14:04:18 +0100

    Create sidebar.php

---
+++ back-end/resources/templates/back/sidebar.php
@@ -0,0 +1,120 @@
+<div class="app-sidebar sidebar-shadow">
+                    <div class="app-header__logo">
+                        <div class="logo-src"></div>
+                        <div class="header__pane ml-auto">
+                            <div>
+                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                                    <span class="hamburger-box">
+                                        <span class="hamburger-inner"></span>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="app-header__mobile-menu">
+                        <div>
+                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="app-header__menu">
+                        <span>
+                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                                <span class="btn-icon-wrapper">
+                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                                </span>
+                            </button>
+                        </span>
+                    </div>    
+                    <div class="scrollbar-sidebar">
+                        <div class="app-sidebar__inner">
+                            <ul class="vertical-nav-menu">
+                                <li class="app-sidebar__heading">Dashboards</li>
+                                <li>
+                                    <a href="index.php" class="mm-active">
+                                        <i class="metismenu-icon pe-7s-home"></i>
+                                        Analytics Dashboard
+                                    </a>
+                                </li>
+                                
+                                
+                                <li class="app-sidebar__heading">Activities</li>
+                                <li>
+                                    <a href="#">
+                                        <i class="metismenu-icon pe-7s-photo-gallery"></i>
+                                        Projects
+                                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="index.php?create_Projects">
+                                                <i class="metismenu-icon">
+                                                </i>Create new project
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="index.php?manage_projects">
+                                                <i class="metismenu-icon">
+                                                </i>Manage projects
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                               
+                                <li class="app-sidebar__heading">communication</li>
+                                <li>
+                                    <a href="#">
+                                        <i class="metismenu-icon pe-7s-users"></i>
+                                        Testimonials
+                                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="index.php?create_testimonials">
+                                                <i class="metismenu-icon"></i>
+                                                Create client member
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="index.php?manage_testimonials">
+                                                <i class="metismenu-icon">
+                                                </i>Manage client member
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="app-sidebar__heading">Admin Area</li>
+                                <li>
+                                    <a href="#">
+                                        <i class="metismenu-icon pe-7s-users"></i>
+                                        Users
+                                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="index.php?add_user">
+                                                <i class="metismenu-icon"></i>
+                                                Add user
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="index.php?manage_users">
+                                                <i class="metismenu-icon">
+                                                </i>Manage users
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="index.php?change_pass">
+                                        <i class="metismenu-icon pe-7s-door-lock"></i>
+                                        Change password
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
