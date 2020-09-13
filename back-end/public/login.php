8924716de4da463b2f4860ed829ccd6e0e076378
Salma Belassiri <salmabelassiri@gmail.com>
2020-09-13 14:26:22 +0100

    Update login.php

--- back-end/public/login.php
+++ back-end/public/login.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>2AI | Login Ibtikarat arab agency</title>
+    <title> Login Salma Belassiri</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
