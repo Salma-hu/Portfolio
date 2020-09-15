e5c5f378b1d4e4028164cd33c80c5bdbb3324e70
Salma Belassiri <salmabelassiri@gmail.com>
2020-09-15 22:25:57 +0100

    Update index.php

--- back-end/public/index.php
+++ back-end/public/index.php
@@ -78,7 +78,6 @@
     </nav>
 	
     <section id="home" class="main-banner parallaxie" style="background: url('images/back.jpg')">
-        <img class="photo" src="images/téléchargement.jpg" alt="">
 		<div class="heading">
 			<h1>hello i'm Salma</h1>
 			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
@@ -119,7 +118,7 @@
 		<div class="container">
 			<div class="section-title text-left">
                 <h3>My projects</h3>
-                <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus.</p>
+                <p>An overview of the projects that you have worked on.</p>
             </div><!-- end title -->
             <div class="gallery-list row">
 			
@@ -148,7 +147,10 @@
     </div><!-- end section -->
 	
 
+    <?php send_mail_php() ?>
 
+<p class="contactForm__alertmsg alertMsg"><?php if(isset($send)) echo $send; ?></p>
+<p class="contactForm__alertmsg alertMsg"><?php if(isset($notsent)) echo $notsent; ?></p>
     <div id="contact" class="section db">
         <div class="container">
             <div class="section-title text-left">
@@ -160,32 +162,37 @@
                 <div class="col-md-12">
                     <div class="contact_form">
                         <div id="message"></div>
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                        <form method="POST" action="index.php" id="contactForm" name="sentMessage" novalidate="novalidate">
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group">
-										<input class="form-control" id="name" type="text" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name.">
-										<p class="help-block text-danger"></p>
+                                        <input class="form-control" name="name" id="name" type="text" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name.">
+                                        <p class="help-block text-danger"></p>
+                                        <p class="contactForm__alertmsg"><?php if(isset($name_error)) echo $name_error; ?></p>
 									</div>
 									<div class="form-group">
-										<input class="form-control" id="email" type="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email address.">
-										<p class="help-block text-danger"></p>
+										<input class="form-control" name="email" id="email" type="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email address.">
+                                        <p class="help-block text-danger"></p>
+                                        <p class="contactForm__alertmsg"><?php if(isset($email_error)) echo $email_error; ?></p>
 									</div>
 									<div class="form-group">
-										<input class="form-control" id="phone" type="tel" placeholder="Your Phone" required="required" data-validation-required-message="Please enter your phone number.">
-										<p class="help-block text-danger"></p>
+										<input class="form-control" name="phone" id="phone" type="tel" placeholder="Your Phone" required="required" data-validation-required-message="Please enter your phone number.">
+                                        <p class="help-block text-danger"></p>
+                                        <p class="contactForm__alertmsg"><?php if(isset($subject_error)) echo $subject_error; ?></p>
 									</div>
 								</div>
 								<div class="col-md-6">
 									<div class="form-group">
-										<textarea class="form-control" id="message" placeholder="Your Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-										<p class="help-block text-danger"></p>
+										<textarea class="form-control" name="message" id="message" placeholder="Your Message" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                        <p class="help-block text-danger"></p>
+                                        <p class="contactForm__alertmsg"><?php if(isset($message_error)) echo $message_error; ?></p>
 									</div>
 								</div>
 								<div class="clearfix"></div>
 								<div class="col-lg-12 text-center">
-									<div id="success"></div>
-									<button id="sendMessageButton" class="sim-btn btn-hover-new" data-text="Send Message" type="submit">Send Message</button>
+                                    <div id="success"></div>
+                                    
+									<input id="submit" name="submit" class="sim-btn btn-hover-new" data-text="Send Message" type="submit" value="Envoyer">
 								</div>
 							</div>
 						</form>
